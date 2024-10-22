fc1e3435f68a0816e1a841157d07d902f0ed3c99
sachin bagaria <your.email@example.com>
2024-10-22 23:15:49 +0530

    add dashboard

--- src/features/dashboard/index.js
+++ src/features/dashboard/index.js
@@ -1,78 +1,176 @@
-import DashboardStats from './components/DashboardStats'
-import AmountStats from './components/AmountStats'
-import PageStats from './components/PageStats'
+import DashboardStats from "./components/DashboardStats";
+import AmountStats from "./components/AmountStats";
+import PageStats from "./components/PageStats";
 
-import UserGroupIcon  from '@heroicons/react/24/outline/UserGroupIcon'
-import UsersIcon  from '@heroicons/react/24/outline/UsersIcon'
-import CircleStackIcon  from '@heroicons/react/24/outline/CircleStackIcon'
-import CreditCardIcon  from '@heroicons/react/24/outline/CreditCardIcon'
-import UserChannels from './components/UserChannels'
-import LineChart from './components/LineChart'
-import BarChart from './components/BarChart'
-import DashboardTopBar from './components/DashboardTopBar'
-import { useDispatch } from 'react-redux'
-import {showNotification} from '../common/headerSlice'
-import DoughnutChart from './components/DoughnutChart'
-import { useState } from 'react'
+import UserGroupIcon from "@heroicons/react/24/outline/UserGroupIcon";
+import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
+import CircleStackIcon from "@heroicons/react/24/outline/CircleStackIcon";
+import CreditCardIcon from "@heroicons/react/24/outline/CreditCardIcon";
+import UserChannels from "./components/UserChannels";
+import LineChart from "./components/LineChart";
+import BarChart from "./components/BarChart";
+import DashboardTopBar from "./components/DashboardTopBar";
+import { useDispatch, useSelector } from "react-redux";
+import { showNotification } from "../common/headerSlice";
+import DoughnutChart from "./components/DoughnutChart";
+import { useEffect, useState } from "react";
+import TitleCard from "../../components/Cards/TitleCard";
+import { TopSideButtons } from "../../utils/topSideButtons";
+import { getLeadsContent } from "../leads/leadSlice";
+import { openModal } from "../common/modalSlice";
+import {
+  CONFIRMATION_MODAL_CLOSE_TYPES,
+  MODAL_BODY_TYPES,
+} from "../../utils/globalConstantUtil";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import moment from "moment";
 
 const statsData = [
-    {title : "New Users", value : "34.7k", icon : <UserGroupIcon className='w-8 h-8'/>, description : "↗︎ 2300 (22%)"},
-    {title : "Total Sales", value : "$34,545", icon : <CreditCardIcon className='w-8 h-8'/>, description : "Current month"},
-    {title : "Pending Leads", value : "450", icon : <CircleStackIcon className='w-8 h-8'/>, description : "50 in hot leads"},
-    {title : "Active Users", value : "5.6k", icon : <UsersIcon className='w-8 h-8'/>, description : "↙ 300 (18%)"},
-]
+  {
+    title: "New Users",
+    value: "34.7k",
+    icon: <UserGroupIcon className="w-8 h-8" />,
+    description: "↗︎ 2300 (22%)",
+  },
+  {
+    title: "Total Sales",
+    value: "$34,545",
+    icon: <CreditCardIcon className="w-8 h-8" />,
+    description: "Current month",
+  },
+  {
+    title: "Pending Leads",
+    value: "450",
+    icon: <CircleStackIcon className="w-8 h-8" />,
+    description: "50 in hot leads",
+  },
+  {
+    title: "Active Users",
+    value: "5.6k",
+    icon: <UsersIcon className="w-8 h-8" />,
+    description: "↙ 300 (18%)",
+  },
+];
 
+function Dashboard() {
+  const dispatch = useDispatch();
 
+  const updateDashboardPeriod = (newRange) => {
+    // Dashboard range changed, write code to refresh your values
+    dispatch(
+      showNotification({
+        message: `Period updated to ${newRange.startDate} to ${newRange.endDate}`,
+        status: 1,
+      })
+    );
+  };
 
-function Dashboard(){
+  const { leads } = useSelector((state) => state.lead);
 
-    const dispatch = useDispatch()
- 
+  useEffect(() => {
+    dispatch(getLeadsContent());
+  }, []);
 
-    const updateDashboardPeriod = (newRange) => {
-        // Dashboard range changed, write code to refresh your values
-        dispatch(showNotification({message : `Period updated to ${newRange.startDate} to ${newRange.endDate}`, status : 1}))
-    }
+  const getDummyStatus = (index) => {
+    if (index % 5 === 0) return <div className="badge">Not Interested</div>;
+    else if (index % 5 === 1)
+      return <div className="badge badge-primary">In Progress</div>;
+    else if (index % 5 === 2)
+      return <div className="badge badge-secondary">Sold</div>;
+    else if (index % 5 === 3)
+      return <div className="badge badge-accent">Need Followup</div>;
+    else return <div className="badge badge-ghost">Open</div>;
+  };
 
-    return(
-        <>
-        {/** ---------------------- Select Period Content ------------------------- */}
-            <DashboardTopBar updateDashboardPeriod={updateDashboardPeriod}/>
-        
-        {/** ---------------------- Different stats content 1 ------------------------- */}
-            <div className="grid lg:grid-cols-4 mt-2 md:grid-cols-2 grid-cols-1 gap-6">
-                {
-                    statsData.map((d, k) => {
-                        return (
-                            <DashboardStats key={k} {...d} colorIndex={k}/>
-                        )
-                    })
-                }
-            </div>
+  const deleteCurrentLead = (index) => {
+    dispatch(
+      openModal({
+        title: "Confirmation",
+        bodyType: MODAL_BODY_TYPES.CONFIRMATION,
+        extraObject: {
+          message: `Are you sure you want to delete this lead?`,
+          type: CONFIRMATION_MODAL_CLOSE_TYPES.LEAD_DELETE,
+          index,
+        },
+      })
+    );
+  };
 
+  return (
+    <>
+      {/** ---------------------- Select Period Content ------------------------- */}
+      <DashboardTopBar updateDashboardPeriod={updateDashboardPeriod} />
 
-
-        {/** ---------------------- Different charts ------------------------- */}
-            <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
-                <LineChart />
-                <BarChart />
-            </div>
-            
-        {/** ---------------------- Different stats content 2 ------------------------- */}
-        
-            <div className="grid lg:grid-cols-2 mt-10 grid-cols-1 gap-6">
-                <AmountStats />
-                <PageStats />
-            </div>
-
-        {/** ---------------------- User source channels table  ------------------------- */}
-        
-            <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
-                <UserChannels />
-                <DoughnutChart />
-            </div>
-        </>
-    )
+      {/** ---------------------- Different stats content 1 ------------------------- */}
+      <div className="grid lg:grid-cols-4 mt-2 md:grid-cols-2 grid-cols-1 gap-6">
+        {statsData.map((d, k) => {
+          return <DashboardStats key={k} {...d} colorIndex={k} />;
+        })}
+      </div>
+      <div className="mt-7">
+        <TitleCard
+          title="Current Leads"
+          topMargin="mt-2"
+          TopSideButtons={<TopSideButtons />}
+        >
+          {/* Leads List in table format loaded from slice after api call */}
+          <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Item Id</th>
+                  <th>Created At</th>
+                  <th>Status</th>
+                  <th>Assigned To</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {leads.map((l, k) => {
+                  return (
+                    <tr key={k}>
+                      <td>
+                        <div className="flex items-center space-x-3">
+                          {/* <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                              <img src={l.avatar} alt="Avatar" />
+                            </div>
+                          </div> */}
+                          <div>
+                            <div className="font-bold">{l.first_name}</div>
+                            <div className="text-sm opacity-50">
+                              {l.last_name}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td>{l.email}</td>
+                      <td>
+                        {moment(new Date())
+                          .add(-5 * (k + 2), "days")
+                          .format("DD MMM YY")}
+                      </td>
+                      <td>{getDummyStatus(k)}</td>
+                      <td>{l.last_name}</td>
+                      <td>
+                        <button
+                          className="btn btn-square btn-ghost"
+                          onClick={() => deleteCurrentLead(k)}
+                        >
+                          <TrashIcon className="w-5" />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </TitleCard>
+      </div>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
